386e746c1b4a11545947900ef05ca41274a70f49
qz <quadratz@proton.me>
2025-03-13 17:20:34 +0700

    export types
    Signed-off-by: qz <quadratz@proton.me>

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
 export { fromTelegramEntity } from "./src/from_telegram_entity";
+export type { Message } from "@grammyjs/types";
+export type { VFile } from "vfile";
+export type { Root } from "tgast";
